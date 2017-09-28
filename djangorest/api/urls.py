9b5ae28b29d60196be4af1b87856fe7c3d9abb51
Robley Gori <robleyadrian@gmail.com>
2017-09-28 12:53:09 +0300

    Initial Commit

---
+++ djangorest/api/urls.py
@@ -0,0 +1,18 @@
+from django.conf.urls import url, include
+from rest_framework.urlpatterns import format_suffix_patterns
+from rest_framework.documentation import include_docs_urls
+from rest_framework.authtoken.views import obtain_auth_token
+
+from .views import CreateView, DetailsView
+
+urlpatterns = {
+    url(r'^auth/', include('rest_framework.urls', namespace='rest_framework')),
+    url(r'^bucketlists/$', CreateView.as_view(), name="create"),
+    url(r'^bucketlists/(?P<pk>[0-9]+)/$', DetailsView.as_view(), name="details"),
+    # url(r'^bucketlists/(?P<pk>[0-9]+)/items$', CreateItemView.as_view(), name="create-item"),
+    # url(r'^bucketlists/(?P<pk>[0-9]+)/items/(?P<id>[0-9]+)$', ItemsView.as_view(), name="items"),
+    url(r'^docs/', include_docs_urls(title='Todo API', description='RESTful API for Bucketlists')),
+    url(r'^get-token/', obtain_auth_token),
+}
+
+urlpatterns = format_suffix_patterns(urlpatterns)
---
+++ djangorest/api/views.py
@@ -0,0 +1,48 @@
+from rest_framework import generics
+from rest_framework import permissions
+
+from .permissions import IsOwner
+from .serializers import BucketlistSerializer
+from .models import Bucketlist
+
+class CreateView(generics.ListCreateAPIView):
+    """This class defines the create behavior of our rest api."""
+    queryset = Bucketlist.objects.all()
+    serializer_class = BucketlistSerializer
+    permission_classes = (permissions.IsAuthenticated, IsOwner)
+
+    def perform_create(self, serializer):
+        """Save the post data when creating a new bucketlist."""
+        serializer.save(owner=self.request.user)
+
+
+class DetailsView(generics.RetrieveUpdateDestroyAPIView):
+    """This class handles the http GET, PUT and DELETE requests."""
+
+    queryset = Bucketlist.objects.all()
+    serializer_class = BucketlistSerializer
+    permission_classes = (
+        permissions.IsAuthenticated,
+        IsOwner)
+
+
+# class CreateItemView(generics.ListCreateAPIView):
+#     """This class defines the create behavior of our rest api."""
+#     queryset = Item.objects.all()
+#     serializer_class = ItemSerializer
+#     permission_classes = (permissions.IsAuthenticated, IsOwner)
+
+#     def perform_create(self, serializer):
+#         """Save the post data when creating a new bucketlist."""
+#         bucketlist = Bucketlist.objects.all()
+#         serializer.save(owner=self.request.user, bucketlist=bucketlist)
+
+
+# class ItemsView(generics.RetrieveUpdateDestroyAPIView):
+#     """This class handles the http GET, PUT and DELETE requests."""
+
+#     queryset = Bucketlist.objects.all()
+#     serializer_class = ItemSerializer
+#     permission_classes = (
+#         permissions.IsAuthenticated,
+#         IsOwner)
