85b51bd29a8fe7323e84597ca11e7e7b6ea05245
Robley Gori <robleyadrian@gmail.com>
2017-11-20 15:23:41 +0300

    [feature] Uncomment views for items

--- djangorest/api/views.py
+++ djangorest/api/views.py
@@ -2,8 +2,8 @@ from rest_framework import generics
 from rest_framework import permissions
 
 from .permissions import IsOwner
-from .serializers import BucketlistSerializer
-from .models import Bucketlist
+from .serializers import BucketlistSerializer, ItemSerializer
+from .models import Bucketlist, Item
 
 class CreateView(generics.ListCreateAPIView):
     """This class defines the create behavior of our rest api."""
@@ -26,23 +26,23 @@ class DetailsView(generics.RetrieveUpdateDestroyAPIView):
         IsOwner)
 
 
-# class CreateItemView(generics.ListCreateAPIView):
-#     """This class defines the create behavior of our rest api."""
-#     queryset = Item.objects.all()
-#     serializer_class = ItemSerializer
-#     permission_classes = (permissions.IsAuthenticated, IsOwner)
+class CreateItemView(generics.ListCreateAPIView):
+    """This class defines the create behavior of our rest api."""
+    queryset = Item.objects.all()
+    serializer_class = ItemSerializer
+    permission_classes = (permissions.IsAuthenticated, IsOwner)
 
-#     def perform_create(self, serializer):
-#         """Save the post data when creating a new bucketlist."""
-#         bucketlist = Bucketlist.objects.all()
-#         serializer.save(owner=self.request.user, bucketlist=bucketlist)
+    def perform_create(self, serializer):
+        """Save the post data when creating a new item."""
+        bucketlist = Bucketlist.objects.get(pk=self.kwargs['pk'])
+        serializer.save(owner=self.request.user, bucketlist=bucketlist)
 
 
-# class ItemsView(generics.RetrieveUpdateDestroyAPIView):
-#     """This class handles the http GET, PUT and DELETE requests."""
+class ItemsView(generics.RetrieveUpdateDestroyAPIView):
+    """This class handles the http GET, PUT and DELETE requests."""
 
-#     queryset = Bucketlist.objects.all()
-#     serializer_class = ItemSerializer
-#     permission_classes = (
-#         permissions.IsAuthenticated,
-#         IsOwner)
+    queryset = Item.objects.all()
+    serializer_class = ItemSerializer
+    permission_classes = (
+        permissions.IsAuthenticated,
+        IsOwner)
