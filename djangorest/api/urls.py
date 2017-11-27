8a4b9e8000516b45ca8d20be1a430a8be10dd1d0
Robley Gori <robleyadrian@gmail.com>
2017-11-27 10:46:49 +0300

    [Feature] Remove unused urls and update views.

--- djangorest/api/urls.py
+++ djangorest/api/urls.py
@@ -3,16 +3,17 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from rest_framework.documentation import include_docs_urls
 from rest_framework.authtoken.views import obtain_auth_token
 
-from .views import CreateView, DetailsView, CreateItemView, ItemsView
+from .views import BucketlistView, BucketlistDetailsView, ItemView, ItemsDetailsView, UserView, UserDetailsView
 
 urlpatterns = {
-    url(r'^auth/', include('rest_framework.urls', namespace='rest_framework')),
-    url(r'^bucketlists/$', CreateView.as_view(), name="create"),
-    url(r'^bucketlists/(?P<pk>[0-9]+)/$', DetailsView.as_view(), name="details"),
-    url(r'^bucketlists/(?P<pk>[0-9]+)/items/$', CreateItemView.as_view(), name="create-item"),
-    url(r'^bucketlists/(?P<pk>[0-9]+)/items/(?P<id>[0-9]+)$', ItemsView.as_view(), name="items"),
+    url(r'^accounts/', include('rest_registration.api.urls')),
+    url(r'^bucketlists/$', BucketlistView.as_view(), name="create"),
+    url(r'^bucketlists/(?P<pk>[0-9]+)/$', BucketlistDetailsView.as_view(), name="details"),
+    url(r'^bucketlists/(?P<pk>[0-9]+)/items/$', ItemView.as_view(), name="create-item"),
+    url(r'^bucketlists/(?P<pk>[0-9]+)/items/(?P<id>[0-9]+)$', ItemsDetailsView.as_view(), name="items"),
+    url(r'^users/(?P<pk>[0-9]+)/$', UserDetailsView.as_view(), name="user-details"),
     url(r'^get-token/$', obtain_auth_token),
-    url(r'^docs/', include('rest_framework_docs.urls')),
+    url(r'^docs/', include('rest_framework_docs.urls'))
 }
 
 urlpatterns = format_suffix_patterns(urlpatterns)
