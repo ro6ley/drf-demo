5bdec5dd4cb04e16d93c39e2db7463b92b3f873c
Robley Gori <robleyadrian@gmail.com>
2017-11-27 10:46:17 +0300

    [Feature] Register models to appear on the admin section.

--- djangorest/api/admin.py
+++ djangorest/api/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from .models import Bucketlist, Item
+
+admin.site.site_header = "Bucketlist Administration"
+admin.site.site_title = "Bucketlist Administration"
+
 # Register your models here.
+admin.site.register(Bucketlist)
+admin.site.register(Item)
