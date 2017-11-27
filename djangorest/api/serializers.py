3100dc68be4e053f1b5f3406aaa5bfe376bc3f07
Robley Gori <robleyadrian@gmail.com>
2017-11-27 10:48:35 +0300

    [Feature] Add a serializer for user information.

--- djangorest/api/serializers.py
+++ djangorest/api/serializers.py
@@ -1,20 +1,22 @@
+from django.contrib.auth.models import User
 from rest_framework import serializers
 from .models import Bucketlist, Item
 
+
 class BucketlistSerializer(serializers.ModelSerializer):
-    """Serializer to map the Model instance into JSON format."""
+    """Serializer to map the Bucketlist Model instance into JSON format."""
 
     owner = serializers.ReadOnlyField(source='owner.username')
 
     class Meta:
         """Meta class to map serializer's fields with the model fields."""
         model = Bucketlist
-        fields = ('id', 'name', 'owner', 'date_created', 'date_modified')
+        fields = ('id', 'name', 'owner', 'items', 'date_created', 'date_modified')
         read_only_fields = ('date_created', 'date_modified')
 
 
 class ItemSerializer(serializers.ModelSerializer):
-    """Serializer to map the Model instance into JSON format."""
+    """Serializer to map the Item Model instance into JSON format."""
 
     owner = serializers.ReadOnlyField(source='owner.username')
 
@@ -23,3 +25,15 @@ class ItemSerializer(serializers.ModelSerializer):
         model = Item
         fields = ('id', 'name', 'owner', 'bucketlist', 'date_created', 'date_modified')
         read_only_fields = ('date_created', 'date_modified')
+
+
+class UserSerializer(serializers.ModelSerializer):
+    """Serializer to map the User model"""
+    bucketlists = serializers.PrimaryKeyRelatedField(many=True, queryset=Bucketlist.objects.all())
+
+    class Meta:
+        """Meta class to map serializer's fields with the model fields."""
+        model = User
+        fields = ('username', 'first_name', 'last_name', 'email', 'bucketlists')
+        write_only_fields = ('password')
+        read_only_fields = ('is_staff', 'is_superuser', 'is_active', 'date_joined',)
